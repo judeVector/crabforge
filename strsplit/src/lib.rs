856383ad6b64b460ac169241b80f5286a50cf59e
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2026-02-12 00:52:43 +0100

    modify the next method

--- strsplit/src/lib.rs
+++ strsplit/src/lib.rs
@@ -15,7 +15,29 @@ impl<'a> StrSplit<'a> {
 impl<'a> Iterator for StrSplit<'a> {
     type Item = &'a str;
 
-    fn next(&mut self) -> Option<Self::Item> {}
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(ref mut remainder) = self.remainder {
+            if let Some(next_delim) = remainder.find(self.delimiter) {
+                let until_delimiter = &remainder[..next_delim];
+                *remainder = &remainder[(next_delim + self.delimiter.len())..];
+                Some(until_delimiter)
+            } else {
+                self.remainder.take()
+            }
+        } else {
+            None
+        }
+    }
+    // fn next(&mut self) -> Option<Self::Item> {
+    //     let remainder = self.remainder.as_mut()?;
+    //     if let Some(next_delim) = remainder.find(self.delimiter) {
+    //         let until_delimiter = &remainder[..next_delim];
+    //         *remainder = &remainder[(next_delim + self.delimiter.len())..];
+    //         Some(until_delimiter)
+    //     } else {
+    //         self.remainder.take()
+    //     }
+    // }
 }
 
 #[cfg(test)]
@@ -25,7 +47,14 @@ mod it_works {
     #[test]
     fn head() {
         let haystack = "a b c d e";
-        let letters: Vec<_> = StrSplit::new(&haystack, "").collect();
+        let letters: Vec<_> = StrSplit::new(&haystack, " ").collect();
         assert_eq!(letters, vec!["a", "b", "c", "d", "e"]);
+    }
+
+    #[test]
+    fn tail() {
+        let haystack = "a b c d ";
+        let letters: Vec<_> = StrSplit::new(&haystack, " ").collect();
+        assert_eq!(letters, vec!["a", "b", "c", "d", ""]);
     }
 }
