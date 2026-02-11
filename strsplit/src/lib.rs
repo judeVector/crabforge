ba88014e51a94ba7990076f83199692fd18af52a
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2026-02-11 22:36:23 +0100

    add test and lifetime specifiers

--- strsplit/src/lib.rs
+++ strsplit/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+struct StrSplit<'a> {
+    remainder: Option<&'a str>,
+    delimiter: &'a str,
+}
+
+impl<'a> StrSplit<'a> {
+    fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+        Self {
+            remainder: Some(haystack),
+            delimiter,
+        }
+    }
+}
+
+impl<'a> Iterator for StrSplit<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {}
 }
 
 #[cfg(test)]
-mod tests {
+mod it_works {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn head() {
+        let haystack = "a b c d e";
+        let letters: Vec<_> = StrSplit::new(&haystack, "").collect();
+        assert_eq!(letters, vec!["a", "b", "c", "d", "e"]);
     }
 }
