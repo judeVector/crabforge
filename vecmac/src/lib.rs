a48b8e438e10de3ccbc24db351afd94c2f69893a
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2026-02-23 19:51:06 +0100

    add clone take test and count array

--- vecmac/src/lib.rs
+++ vecmac/src/lib.rs
@@ -1,5 +1,3 @@
-use std::i32;
-
 #[macro_export]
 macro_rules! avec {
     () => {
@@ -9,21 +7,15 @@ macro_rules! avec {
         let mut vs = Vec::new();
         $(vs.push($element);)*
         vs
-    }}
-}
-
-trait MaxValue {
-    fn max_value() -> Self;
-}
-
-macro_rules! max_impl {
-    ($t:ty) => {
-        impl $crate::MaxValue for $t {
-            fn max_value() -> Self {
-                <$t>::MAX
-            }
+    }};
+    ($element:expr; $count:expr) => {{
+        let mut vs = Vec::new();
+        let x = $element;
+        for _ in 0..$count {
+            vs.push(x.clone())
         }
-    };
+        vs
+    }}
 }
 
 #[cfg(test)]
@@ -31,7 +23,6 @@ mod test {
 
     #[test]
     fn empty_vec() {
-        max_impl!(i32);
         let x: Vec<u32> = avec![];
         assert!(x.is_empty())
     }
@@ -63,4 +54,30 @@ mod test {
         assert!(!x.is_empty());
         assert_eq!(x.len(), 4);
     }
+
+    #[test]
+    fn clone_2() {
+        let x = avec![42; 2];
+        assert!(!x.is_empty());
+        assert_eq!(x.len(), 2);
+        assert_eq!(x[0], 42);
+        assert_eq!(x[1], 42);
+    }
+
+    #[test]
+    fn clone_take() {
+        let mut x = Some(42);
+        let x = avec![x.take().unwrap(); 2];
+        assert!(!x.is_empty());
+        assert_eq!(x.len(), 2);
+        assert_eq!(x[0], 42);
+        assert_eq!(x[1], 42);
+    }
 }
+
+///
+/// ```compile_fail
+/// let x: Vec<u32> = vecmac::avec![42; "foo"];
+/// ```
+#[allow(dead_code)]
+struct CompileFailTest;
